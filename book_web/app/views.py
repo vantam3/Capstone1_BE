2005faba82c901b7036d8ed1f3fd00bf3f946fd4
CyberseSorceress <nguyenhalong102@gmail.com>
2024-10-22 02:56:58 +0700

    add feature register,login

---
+++ book_web/app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
+
+def home(request):
+    return HttpResponse("Bookquest")
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Đăng ký thành công!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Đăng nhập thành công!')
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'app/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'Bạn đã đăng xuất.')
+    return redirect('login')
+
