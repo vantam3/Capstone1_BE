9ea4baa7b7f61c0b522d92fd5f2b82703b94f83d
Tam Tran <vtam0805@gmail.com>
2024-11-18 01:10:05 +0700

    fix review

--- book_web/app/urls.py
+++ book_web/app/urls.py
@@ -3,6 +3,10 @@
 from django.urls import path
 from . import views
 from .views import BookSearchAPIView
+from .views import list_users, list_books
+from .views import create_user, update_user, delete_user, fetch_and_add_books
+
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -21,6 +25,19 @@ urlpatterns = [
     # API endpoints for reviews
     path('api/books/<int:book_id>/add_review/', views.add_review, name='add_review'),
     path('api/books/<int:book_id>/reviews/', views.get_book_reviews, name='get_book_reviews'),
+    
+    
+    #admin
+    path('api/admin/users/', list_users, name='list_users'),
+    path('api/admin/books/', list_books, name='list_books'),
+    
+    path('api/admin/users/create/', create_user, name='create_user'),
+    path('api/admin/users/<int:user_id>/update/', update_user, name='update_user'),
+    path('api/admin/users/<int:user_id>/delete/', delete_user, name='delete_user'),
+    
+    #add sach
+    path('api/admin/fetch-books/', fetch_and_add_books, name='fetch_books'),
+
 ]
 
 
